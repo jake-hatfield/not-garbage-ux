147eca4d6a31f2b2e43a53c38b06053d9d86a8ef
Jacob Hatfield <jacobmhatfield@gmail.com>
2020-05-14 13:44:08 -0500

    calendly fixes?

--- src/components/calendly.js
+++ src/components/calendly.js
@@ -23,14 +23,23 @@ class CalendlyEmbed extends React.Component {
     const { account, eventName, calendlyActive, setCalendlyActive } = this.props
     return (
       <div
-        id="schedule_form"
-        className={`${calendlyActive ? `block` : `hidden`} calendly-overlay`}
+        id="invisible-div"
+        className={`${
+          calendlyActive ? `block lg:flex lg:items-center` : `hidden`
+        } calendly-overlay overflow-y-hidden`}
+        onClick={() => setCalendlyActive(!calendlyActive)}
       >
         <div
-          className="calendly-inline-widget"
+          className="calendly-inline-widget calendly-styles w-full z-40"
           data-url={this.buildCalendlyUrl(account, eventName)}
-          style={{ minWidth: "480px", height: "640px" }}
-        />
+        >
+          <button
+            onClick={() => setCalendlyActive(!calendlyActive)}
+            className="close-popup p-3 rounded-lg focus:outline-none focus:shadow-outline"
+          >
+            <Close className="h-4 w-4" />
+          </button>
+        </div>
       </div>
     )
   }
@@ -62,12 +71,12 @@ export default CalendlyEmbed
 //     }
 //   })
 
-//   // const calendlyAnimation = useSpring({
-//   //   transform: calendlyActive
-//   //     ? `translate3d(0%,0,0)`
-//   //     : `translate3d(-100%,0,0)`,
-//   //   config: config.slow,
-//   // })
+// const calendlyAnimation = useSpring({
+//   transform: calendlyActive
+//     ? `translate3d(0%,0,0)`
+//     : `translate3d(-100%,0,0)`,
+//   config: config.slow,
+// })
 //   const fade = useSpring({ opacity: calendlyActive ? 1 : 0 })
 
 //   return (
